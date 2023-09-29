c6006faa9c0b5bc0bfa5dabb801d9bba63a28bfd
IvanGuculyak <ivan.guculyak@gmail.com>
2023-09-29 13:42:45 +0300

    Added models Position, TaskType, Task and Worker to app tasks

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,51 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.CASCADE,
+        related_name="workers"
+    )
+
+    def __str__(self):
+        return f"{self.username} " \
+               f"Full name: {self.first_name} {self.last_name} " \
+               f"Position: {self.position}"
+
+
+class Task(models.Model):
+    PRIORITY = ("Urgent", "Height", "Medium", "Low")
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    deadline = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(choices=PRIORITY, default="Medium")
+    task_type = models.ForeignKey(
+        TaskType,
+        on_delete=models.CASCADE,
+        related_name="tasks"
+    )
+    assignees = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="tasks"
+    )
+
+    def __str__(self):
+        return f"{self.name} priority: {self.priority}"
+
